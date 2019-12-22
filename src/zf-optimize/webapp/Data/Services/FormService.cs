3eea626c1aa69e6849fb7cef891010bf64819409
CapOfCave <lars.kecker2009@gmail.com>
2019-12-22 18:37:26 +0100

    creating FormService

---
+++ src/zf-optimize/webapp/Data/Services/FormService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using webapp.Data.Entities;
+
+namespace webapp.Data.Services
+{
+    public class FormService
+    {
+        private readonly ZFContext _context;
+
+        public FormService(ZFContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Form>> GetFormsAsync()
+        {
+            return await _context.Forms.ToListAsync();
+        }
+
+        public async Task<Form> GetFormAsync(int id)
+        {
+            return await _context.Forms
+              .FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task<Form> CreateFormAsync(Form form)
+        {
+            _context.Forms.Add(form);
+            await _context.SaveChangesAsync();
+            return await GetFormAsync(form.Id);
+        }
+
+        public async Task DeleteForm(Form form)
+        {
+            _context.Forms.Remove(form);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
