84c6491de32c58ccf0e54e1dbbe1455999d5aab1
Nico Kranz <nico@kranz.space>
2019-12-31 17:39:49 +0100

    wip order + orderproducts

---
+++ webapp/Pages/OrderComponents/Years.razor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using webapp.Data;
+
+namespace webapp.Pages.OrderComponents
+{
+    public partial class Years : ComponentBase
+    {
+        [Inject] private ZFContext zfContext { get; set; }
+        [Inject] private NavigationManager navigationManager { get; set; }
+
+
+        private protected List<int> YearList { get; set; }
+
+        private protected int Year { get; set; }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            YearList = await zfContext.Orders.Select(o => o.Date.Year).Distinct().ToListAsync();
+            StateHasChanged();
+        }
+
+        private protected void RedirectToOrder()
+        {
+            if (Year != default)
+                navigationManager.NavigateTo($"/orders/{Year}");
+        }
+    }
+}
